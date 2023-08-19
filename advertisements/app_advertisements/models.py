892b98d43c060866933ab5e9f72d270f75e7a9af
PAHAN <bryzgalovpaul@yandex.ru>
2023-08-19 16:42:58 +0300

    'lesson8'

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Advertisement(models.Model):
@@ -14,3 +15,21 @@ class Advertisement(models.Model):
 
     class Meta:
         db_table = 'advertisements'
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style = "color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style = "color: red; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
